be68edb4c1d24532ac48b70ef84ec593da4dde4c
0xShreya26 <shreyamaurya291@gmail.com>
2026-08-21 19:10:42 +0530

    Create comment.c
    comments in c

---
+++ comment.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main( )
+{
+	/*int a=1;
+	printf("%d",a);
+	float b=2;
+	printf("%f",b);
+	char c=3;
+	printf("%c",c);
+	printf("size of int;%zubytes\n", sizeof(int));
+	*/
+	///printf("size of char;%zubytes\n", sizeof(char));
+	//printf("size of float;%zubytes\n", sizeof(float));
+	return 0;
+}
