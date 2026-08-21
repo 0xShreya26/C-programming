10e0feded41c30bd6775e807e55e00b66917c7b2
0xShreya26 <shreyamaurya291@gmail.com>
2026-08-21 19:15:37 +0530

    Create keywords and constant.c

---
+++ keywords and constant.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main( )
+{
+//integer constant 1,2,3
+//real constant 1.2,3.4,5.8
+//character constant @#₹&$¢¥€
+/* keywords ==
+auto	double	int	struct
+break	else	long	switch
+case	enum	register	typedef
+char	extern	return	union
+const	float	short	unsigned
+continue	for	signed	void
+default	goto	sizeof	volatile
+do	if	static	while */
+	return 0;
+}
