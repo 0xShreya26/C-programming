45b03c2dbfe0ce16d29df8f6226a442750184d22
0xShreya26 <shreyamaurya291@gmail.com>
2026-08-22 18:44:58 +0530

    Create area_of_cylinder.c
    area of cylinder

---
+++ area_of_cylinder.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main( )
+{
+    int r, h;
+    printf("enter r");
+    scanf("%d",&r);
+    printf("enter h");
+    scanf("%d",&h);
+	printf("The area of circle is %f",3.14*r*h);   
+	 return 0;
+}
