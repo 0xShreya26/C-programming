b2af403ee938947db079e8b9e52597d8dfae7eb1
0xShreya26 <shreyamaurya291@gmail.com>
2026-08-22 18:33:48 +0530

    Create area_of_triangle.c
    area of triangle

---
+++ area_of_triangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main( )
+{
+	int lenght, breadth;
+	printf("enter lenght ");
+	scanf("%d",& lenght);
+	printf("enter breadth ");
+	scanf("%d",& breadth);
+	printf("The area of triangle is %d",lenght*breadth);    return 0;
+}
