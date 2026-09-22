3819169841d9bdf1e7912776a3f56153cdbace94
0xShreya26 <shreyamaurya291@gmail.com>
2026-09-23 00:53:23 +0530

    Update month.c

--- month.c
+++ month.c
@@ -1,32 +1,36 @@
 #include<stdio.h>
-int month ( )
-printf("enter between btw  1 to 12");
-scanf(%d, & x);
-switch month
+int main( )
 {
-	case 1: printf("january 31 days")
-	break;
-    case 2: printf("february 29/28 days")
-	break;
-    case 3: printf("march 31 days")
-	break;
-    case 4: printf("april 30 days")
-	break;
-	case 5: printf("may 31 days")
-	break;
-	case 6: printf("june 30 days")
-	break;
-	case 7: printf("july 31 days")
-	break;
-	case 8: printf("august 31 days")
-	break;
-	case 9: printf("september 30 days")
-	break;
-	case 10: printf("october 31 days")
-	break;
-	case 11: printf("november 30 days")
-	break;
-	case 12: printf("december  31days")
-	break;
-	default : printf("enter value 1 to 12")
+	int month;
+    printf("enter between no  1 to 12");
+    scanf("%d", &month);
+    switch (month)
+    {
+        case 1: printf("january 31 days");
+        break;
+    case 2: printf("february 29/28 days");
+        break;
+    case 3: printf("march 31 days");
+        break;
+    case 4: printf("april 30 days");
+        break;
+        case 5: printf("may 31 days");
+        break;
+        case 6: printf("june 30 days");
+        break;
+        case 7: printf("july 31 days");
+        break;
+        case 8: printf("august 31 days");
+        break;
+        case 9: printf("september 30 days");
+        break;
+        case 10: printf("october 31 days");
+        break;
+        case 11: printf("november 30 days");
+        break;
+        case 12: printf("december  31days");
+        break;
+        default : printf("enter value 1 to 12");
+        return 0;
+    }
 }
